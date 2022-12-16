68eab83830317d1fad680ba043a46b4275f17a05
Jake Hathaway <77807678+th3oth3rjak3@users.noreply.github.com>
2022-12-16 00:49:24 -0800

    created gallery service

---
+++ src/models/services.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+func NewServices(connectionInfo string) (*Services, error) {
+	db, err := gorm.Open("postgres", connectionInfo)
+	if err != nil {
+		return nil, err
+	}
+	db.LogMode(true)
+	return &Services{}, nil
+}
+
+type Services struct {
+	Gallery GalleryService
+	User    UserService
+}
